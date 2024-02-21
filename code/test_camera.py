f11bff79cddfc94d59ea543fb7ed0804426ec826
James Kabbes <james.kabbes@gmail.com>
2024-02-21 11:40:57 -0600

    adding config

--- code/test_camera.py
+++ code/test_camera.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
     total_seconds = 60
     sample_hz = 10
 
-    camera = camera_module.Camera()
+    camera = camera_module.Camera({
+        "show_preview": False
+    })
     start_time = time.time()
 
     while time.time() - start_time < total_seconds:
